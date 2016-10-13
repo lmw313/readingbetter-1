a5e26a276703eebc1894224be14214cb04bc8a64
S401-08 <S401-08@S401-08>
2016-10-13 13:13:09 +0900

    랭킹 앱 컨트롤러 수정

--- src/main/java/kr/ac/readingbetter/controller/app/AppRankController.java
+++ src/main/java/kr/ac/readingbetter/controller/app/AppRankController.java
@@ -1,7 +1,5 @@
 package kr.ac.readingbetter.controller.app;
 
-
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,30 +9,71 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import kr.ac.readingbetter.service.ScoresService;
 import kr.ac.readingbetter.vo.ScoresVo;
 
-
-
 @Controller
 @RequestMapping("/rankapp")
-public class AppRankController {	
+public class AppRankController {
 
 	@Autowired
 	private ScoresService scoresService;
 
+	/* ---------- 한 달 랭킹 시작 ---------- */
+
+	// 한 달 랭킹 상위 5명
 	@ResponseBody
-	@RequestMapping(value = "list", method = RequestMethod.GET)
-	public List<ScoresVo> List(ScoresVo vo) {
-		List<ScoresVo> monthlyRank = scoresService.monthlyRank(vo);
+	@RequestMapping(value = "monthlyrank", method = RequestMethod.GET)
+	public List<ScoresVo> MonthlyRank(ScoresVo vo) {
+		List<ScoresVo> monthlyRank = scoresService.monthlyMainRank(vo);
 		return monthlyRank;
 	}
 
-
+	// 나의 한 달 랭킹
 	@ResponseBody
-	@RequestMapping(value = "myrank", method = RequestMethod.GET)
-	public ScoresVo historyapp(Long memberNo,ScoresVo vo) {
+	@RequestMapping(value = "mymonthlyrank", method = RequestMethod.GET)
+	public ScoresVo MyMonthlyRank(Long memberNo, ScoresVo vo) {
 		vo = scoresService.mymonthlyRank(memberNo);
 		return vo;
+	}
+	/* ---------- 한 달 랭킹 끝 ---------- */
 
+	
+	/* ---------- 학교 랭킹 시작 ---------- */
+
+	// 학교 랭킹 상위 5곳
+	@ResponseBody
+	@RequestMapping(value = "schoolrank", method = RequestMethod.GET)
+	public List<ScoresVo> SchoolRank(ScoresVo vo) {
+		List<ScoresVo> schoolRank = scoresService.mainSchool(vo);
+		return schoolRank;
 	}
 
+	// 나의 학교 랭킹
+	@ResponseBody
+	@RequestMapping(value = "myschoolrank", method = RequestMethod.GET)
+	public ScoresVo MySchoolRank(String id, ScoresVo vo) {
+		vo = scoresService.mySchoolRank(id);
+		return vo;
+	}
 
+	/* ---------- 학교랭킹 끝 ---------- */
+
+	
+	/* ---------- 명예의 전당 시작 ---------- */
+
+	// 명예의 전당 상위 5명
+	@ResponseBody
+	@RequestMapping(value = "honor", method = RequestMethod.GET)
+	public List<ScoresVo> HonorRank(ScoresVo vo) {
+		List<ScoresVo> honorRank = scoresService.mainHonor(vo);
+		return honorRank;
+	}
+
+	// 나의 명예의 전당 랭킹
+	@ResponseBody
+	@RequestMapping(value = "myhonor", method = RequestMethod.GET)
+	public ScoresVo MyHonorRank(Long memberNo, ScoresVo vo) {
+		vo = scoresService.myTotalRank(memberNo);
+		return vo;
+	}
+	
+	/* ---------- 명예의 전당 끝 ---------- */
 }
